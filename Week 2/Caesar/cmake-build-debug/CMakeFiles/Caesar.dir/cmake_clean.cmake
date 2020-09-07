883c19dcc0feed7c8889f73280d22145bde7d1cf
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-09-07 11:59:47 +0300

    2.0

---
+++ Week 2/Caesar/cmake-build-debug/CMakeFiles/Caesar.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Caesar.dir/main.c.obj"
+  "Caesar.exe"
+  "Caesar.exe.manifest"
+  "Caesar.lib"
+  "Caesar.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Caesar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
