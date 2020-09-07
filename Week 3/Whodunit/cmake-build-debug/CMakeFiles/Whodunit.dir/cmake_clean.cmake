0fd17789e1735199297ce44a4ffc75be0bfa60fe
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-09-07 12:01:10 +0300

    3.0

---
+++ Week 3/Whodunit/cmake-build-debug/CMakeFiles/Whodunit.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Whodunit.dir/main.c.obj"
+  "Whodunit.exe"
+  "Whodunit.exe.manifest"
+  "Whodunit.lib"
+  "Whodunit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Whodunit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
