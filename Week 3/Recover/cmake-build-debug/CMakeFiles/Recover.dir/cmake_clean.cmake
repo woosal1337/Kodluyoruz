dc31be2ee3ebcf85c849832f57977de4f7de6629
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-09-07 12:00:33 +0300

    3.0

---
+++ Week 3/Recover/cmake-build-debug/CMakeFiles/Recover.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Recover.dir/main.c.obj"
+  "Recover.exe"
+  "Recover.exe.manifest"
+  "Recover.lib"
+  "Recover.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Recover.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
